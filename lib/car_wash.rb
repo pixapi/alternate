ce58618632b1872a7931771c38a9982ee0a61679
Amara Lovato <amaracuenca@gmail.com>
2016-10-24 16:28:59 -0600

    Car Wash challenge finished

--- lib/car_wash.rb
+++ lib/car_wash.rb
@@ -2,29 +2,37 @@ require 'pry'
 
 class CarWash
 
-  attr_reader :opening_year,
-              :location,
-              :city,
-              :state
+  attr_reader :location_city,
+              :location_state,
+              :opening_year,
+              :no_of_employees
 
   def initialize(location, opening_year)
-    @location = location
+    @location_city = extract_city(location)
+    @location_state = extract_state(location)
     @opening_year = opening_year
+    @no_of_employees = 0
   end
 
-  def location_city
+  def extract_city(location)
     location.split(",")[0]
   end
 
-  def location_state
-    location.split(",")[0]
+  def extract_state(location)
+    location.split(", ")[1]
   end
-  
 
+  def no_of_employees(num)
+    @no_of_employees
+  end
 
+  def hire_employees(num)
+    @no_of_employees += num
+  end
 
-  # def location(location)
-  #   "#{city} + #{state}"
-  # end
+  def provide_year
+    age = 2016 - @opening_year
+    "I am #{age} years old"
+  end
 
 end
--- test/car_wash_test.rb
+++ test/car_wash_test.rb
@@ -2,37 +2,65 @@ gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/car_wash'
+require 'pry'
 
 class CarWashTest < Minitest::Test
+  def test_it_exists
+    assert_instance_of CarWash, CarWash.new("Denver,CO", 2010)
+  end
 
   def test_it_has_a_class
-    car = CarWash.new("Denver,CO", 2010)
+    cw = CarWash.new("Denver,CO", 2010)
 
-    assert_equal CarWash, car.class
+    assert_equal CarWash, cw.class
+  end
+
+  def test_it_can_extract_city
+    cw = CarWash.new("Denver,CO", 2010)
+    location = "Denver, CO"
+
+    assert_equal "Denver", cw.extract_city(location)
+  end
+
+  def test_it_can_extract_state
+    cw = CarWash.new("Denver, CO", 2010)
+    location = "Denver, CO"
+
+    assert_equal "CO", cw.extract_state(location)
+  end
+
+  def test_starts_with_zero_employees
+    cw = CarWash.new("Denver, CO", 2010)
+    num = 0
+
+    assert_equal 0, cw.no_of_employees(num)
+  end
+
+  def test_it_can_hire_employees
+    cw = CarWash.new("Denver, CO", 2010)
+
+    assert_equal 3, cw.hire_employees(3)
+  end
+
+  def test_it_can_hire_employees_in_batches
+    cw = CarWash.new("Denver, CO", 2010)
+
+    cw.hire_employees(3)
+    assert_equal 3, cw.no_of_employees(3)
+
+    cw.hire_employees(4)
+    assert_equal 7, cw.no_of_employees(4)
   end
 
   def test_it_has_an_opening_year
-    car = CarWash.new("Denver,CO", 2010)
+    cw = CarWash.new("Denver,CO", 2010)
 
-    assert_equal 2010, car.opening_year
+    assert_equal 2010, cw.opening_year
   end
 
-  def test_it_has_a_location
-    car = CarWash.new("Denver,CO", 2010)
+  def test_it_can_provide_year
+    cw = CarWash.new("Denver,CO", 2010)
 
-    assert_equal "Denver,CO", car.location
+    assert_equal "I am 6 years old", cw.provide_year
   end
-
-  def test_location_city
-    car = CarWash.new("Denver,CO", 2010)
-
-    assert_equal "Denver", car.location_city
-  end
-
-  def test_num_employees
-    car = CarWash.new("Denver,CO", 2010)
-
-    assert_equal num_employees, car.location_city
-  end
-
 end
